d6143b8c90a2d5e3b0c13e822bb49a0dd928f8a1
windsor-connect <windsortinting@gmail.com>
2026-08-19 17:44:28 -0400

    nav, match modal, public profiles, help, landing

--- lib/SupabaseClie.ts
+++
@@ -1,4 +0,0 @@
-import { createClient, SupabaseClient } from "@supabase/supabase-js";
-
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || "";
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE
